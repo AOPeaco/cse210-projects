e6a740d436c3784cf9eab6ed55f0abe416efe965
AOPeaco <Andrew.o.peacock@gmail.com>
2024-03-01 21:03:35 -0700

    Prep1 and prep2 for week 01 completed.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What score did you get?");
+        string gradeString = Console.ReadLine();
+        int gradePercentage = int.Parse(gradeString);
+
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("You got an A!");
+        }
+        else if (gradePercentage >= 80)
+        {
+            Console.WriteLine("You got a B!");
+        }
+        else if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You got a C!");
+        }
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine("You got a D!");
+        }
+        else if (gradePercentage <= 59)
+        {
+            Console.WriteLine("You got a F!");
+        }
     }
 }
